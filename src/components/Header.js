dca1d9bcfca2d5ee9507ec0d0f1125a049dc4417
Thomas Ender <freisingen@yahoo.com>
2021-05-03 01:59:43 +0200

    React logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Header.css';
+import reactLogo from './React-icon.svg';
 
 const Header = ({ title, description}) => {
     return(
         <div className="Header">
+            <img className="logo" src={reactLogo} alt="react Logo" />
             <div className="headerBG">
                 <h1>{title}</h1>
                 <h2>{description}</h2>
+                
             </div>
 
     
